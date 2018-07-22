9740e155e9eee729e7010619144b0b737d0b5e67
Milhous <milhous@sina.com>
2018-07-22 22:36:29 +0800

    解决: 1. snake 和 snakeManager 代码分离

--- assets/script/component/Snake.js
+++ assets/script/component/Snake.js
@@ -1,10 +1,139 @@
 export default class Snake {
     constructor() {
-        // 数据
-        this._data = {};
-        // 监听列表
-        this._list = [];
-        // 执行栈
-        this._todoStack = [];
+        this._snake = [];
+
+        // 速度
+        this._speed = 0;
+        // 方向向量
+        this._direction = null;
+        // 长度为 1 的标准化过后的向量
+        this._normalize = null;
+
+        // X轴移动最大范围
+        this._rangeX = 0;
+        // Y轴移动最大范围
+        this._rangeY = 0;
+    }
+
+    /*
+     * 初始化
+     * @param (number) snake 
+     * @param (number) initX X轴位置
+     * @param (number) initY Y轴位置
+     * @param (number) rangeX X轴移动范围
+     * @param (number) rangeY Y轴移动范围
+     */
+    init({
+        snake,
+        initX,
+        initY,
+        rangeX,
+        rangeY
+    }) {
+        this._snake = snake;
+
+        this.setMoveRange(rangeX, rangeY);
+
+        this._setInitPosition(initX, initY);
+    }
+
+    /*
+     * 设置初始位置
+     * @param (number) x X轴移动范围
+     * @param (number) y Y轴移动范围
+     */
+    _setInitPosition(x, y) {
+        const _index = 100;
+
+        this._snake.map((part, index) => {
+            part.zIndex = 100 - index;
+            part.x = x;
+            part.y = y;
+        });
+    }
+
+    /*
+     * 设置移动范围
+     * @param (number) rangeX X轴移动最大范围
+     * @param (number) rangeY Y轴移动最大范围
+     */
+    setMoveRange(rangeX, rangeY) {
+        this._rangeX = x;
+        this._rangeY = y;
+    }
+
+    // 更新头部位置
+    _updateHeadPosition() {
+        this._normalize = cc.pNormalize(this._direction);
+
+        const _x = this._head.x + this._normalize.x * this._head.width * this._speed;
+        const _y = this._head.y + this._normalize.y * this._head.width * this._speed;
+        const _vec = this._checkOutRange(_x, _y);
+
+        this._head.setPosition(_vec);
+    }
+
+    // 更新身体位置
+    _updateBodyPosition() {
+        this._bodys.map((body, index, arr) => {
+            const curVec = index === 0 ? this._head.getPosition() : arr[index - 1].getPosition();
+            const preVec = body.getPosition();
+            const subVec = cc.pSub(curVec, preVec);
+
+            if (!body.isMove) {
+                const distance = cc.pDistance(curVec, preVec);
+
+                if (distance >= body.width / 2) {
+                    body.isMove = true;
+                }
+            }
+
+            if (body.isMove) {
+                body.x += subVec.x * this._speed;
+                body.y += subVec.y * this._speed;
+            }
+        });
+    }
+
+    // 更新移动角度
+    _updateHeadAngle() {
+        const angle = cc.pToAngle(this._direction) / Math.PI * 180;
+
+        this._head.rotation = -angle;
+    }
+
+    /*
+     * 检查是否超出边界
+     * @param (number) x X轴位置
+     * @param (number) y Y轴位置
+     */
+    _checkOutRange(x, y) {
+        const _vec = cc.v2(x, y);
+
+        if (_vec.x > this._rangeX) {
+            _vec.x = this._rangeX;
+        }
+
+        if (_vec.x < -this._rangeX) {
+            _vec.x = -this._rangeX;
+        }
+
+        if (_vec.y > this._rangeY) {
+            _vec.y = this._rangeY;
+        }
+
+        if (_vec.y < -this._rangeY) {
+            _vec.y = -this._rangeY;
+        }
+
+        if (Math.abs(_vec.x) >= this._rangeX) {
+            this._direction.x = -this._normalize.x;
+        }
+
+        if (Math.abs(_vec.y) >= this._rangeY) {
+            this._direction.y = -this._normalize.y;
+        }
+
+        return _vec;
     }
 });
--- assets/script/component/SnakeManager.js
+++ assets/script/component/SnakeManager.js
@@ -14,18 +14,6 @@ cc.Class({
         // 身体对象池
         this._bodyPool = null;
 
-        // 速度
-        this._speed = 0;
-        // 方向向量
-        this._direction = {};
-        // 长度为 1 的标准化过后的向量
-        this._normalize = {};
-
-        // X轴移动最大范围
-        this._rangeX = 0;
-        // Y轴移动最大范围
-        this._rangeY = 0;
-
         // 玩家数量
         this._multiplePlayers = 5;
 
@@ -55,8 +43,6 @@ cc.Class({
 
     onLoad() {
         this.init();
-
-        this.setMoveRange(this.node.width / 2, this.node.height / 2);
     },
 
     start() {
@@ -128,20 +114,15 @@ cc.Class({
      */
     create(name = 'default', x = 0, y = 0) {
         const _head = this._getNodeFromPool(this.headPrefab, this._headPool);
-        _head.zIndex = 100;
         this.node.addChild(_head);
 
+        const _bodys = [];
+
+        for (let i = 0; i < this._initLen; i++) {
+            this._addBody();
+        }
 
         const _bodys = this._createBody();
-    },
-
-    // 创建头部
-    _createHead(name) {
-        const _head = this._getNodeFromPool(this.headPrefab, this._headPool);
-        _head.zIndex = 100;
-        this.node.addChild(_head);
-
-        return _head;
     },
 
     // 创建身体
@@ -179,16 +160,6 @@ cc.Class({
         this._speed = speed;
     },
 
-    /*
-     * 设置移动范围
-     * @param (number) x X轴移动最大范围
-     * @param (number) y Y轴移动最大范围
-     */
-    setMoveRange(x, y) {
-        this._rangeX = x;
-        this._rangeY = y;
-    },
-
     // 获取蛇头位置
     getHeadPositon() {
         const _vec = this._head.convertToWorldSpaceAR(cc.Vec2.ZERO);
@@ -199,81 +170,6 @@ cc.Class({
     // 获取活动区域
     getMoveRange() {
         const _vec = cc.v2(this.node.width, this.node.height);
-
-        return _vec;
-    },
-
-    // 更新移动角度
-    _updateHeadAngle() {
-        const angle = cc.pToAngle(this._direction) / Math.PI * 180;
-
-        this._head.rotation = -angle;
-    },
-
-    // 更新头部位置
-    _updateHeadPosition() {
-        this._normalize = cc.pNormalize(this._direction);
-
-        const _x = this._head.x + this._normalize.x * this._head.width * this._speed;
-        const _y = this._head.y + this._normalize.y * this._head.width * this._speed;
-        const _vec = this._checkOutRange(_x, _y);
-
-        this._head.setPosition(_vec);
-    },
-
-    // 更新身体位置
-    _updateBodyPosition() {
-        this._bodys.map((body, index, arr) => {
-            const curVec = index === 0 ? this._head.getPosition() : arr[index - 1].getPosition();
-            const preVec = body.getPosition();
-            const subVec = cc.pSub(curVec, preVec);
-
-            if (!body.isMove) {
-                const distance = cc.pDistance(curVec, preVec);
-
-                if (distance >= body.width / 2) {
-                    body.isMove = true;
-                }
-            }
-
-            if (body.isMove) {
-                body.x += subVec.x * this._speed;
-                body.y += subVec.y * this._speed;
-            }
-        });
-    },
-
-    /*
-     * 检查是否超出边界
-     * @param (number) x X轴位置
-     * @param (number) y Y轴位置
-     */
-    _checkOutRange(x, y) {
-        const _vec = cc.v2(x, y);
-
-        if (_vec.x > this._rangeX) {
-            _vec.x = this._rangeX;
-        }
-
-        if (_vec.x < -this._rangeX) {
-            _vec.x = -this._rangeX;
-        }
-
-        if (_vec.y > this._rangeY) {
-            _vec.y = this._rangeY;
-        }
-
-        if (_vec.y < -this._rangeY) {
-            _vec.y = -this._rangeY;
-        }
-
-        if (Math.abs(_vec.x) >= this._rangeX) {
-            this._direction.x = -this._normalize.x;
-        }
-
-        if (Math.abs(_vec.y) >= this._rangeY) {
-            this._direction.y = -this._normalize.y;
-        }
 
         return _vec;
     }
